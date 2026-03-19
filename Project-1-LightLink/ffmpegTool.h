7e1a3efee3df97d84204a4c962fe7ff52c272a6e
cai-creator <1911439868@qq.com>
2026-03-19 16:13:28 +0800

    初步完成了编码到解码的闭环

--- Project-1-LightLink/ffmpegTool.h
+++ Project-1-LightLink/ffmpegTool.h
@@ -4,5 +4,8 @@
 // Return root directory of the application (string)
 std::string getRootdir_impl();
 
-// 学位
+// 图片合成视频
 void picTovideo_impl(std::string pic_path, std::string video_path, int fps, int size, int Vquality = 18);
+
+// 视频拆帧：将视频文件拆分为帧图片
+int VideotoPic(const char* videoPath, const char* imagePath, const char* imageFormat);
